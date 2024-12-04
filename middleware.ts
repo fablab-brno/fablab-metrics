05f0808f597e6e72622799b3bd3d9c083e1cad2a
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2024-12-04 11:54:41 +0100

    exclude svgs from auth middleware

--- middleware.ts
+++ middleware.ts
@@ -23,5 +23,5 @@ export default async function middleware(req: NextRequest) {
 
 // Routes Middleware should not run on
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$|.*\\.svg$).*)"],
 };
