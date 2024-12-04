2cd6c5c4b01ad407658666fdcdf0bf0896b1e71e
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2024-12-04 14:36:33 +0100

    add logging to api queries

--- app/api/status/route.ts
+++ app/api/status/route.ts
@@ -5,10 +5,14 @@ import path from "node:path";
 export async function GET() {
   const filename = path.join(DATA_PATH, "status.json");
 
+  console.debug(`Reading ${filename}`);
   const data = await fs
     .readFile(filename, "utf8")
     .then((content) => JSON.parse(content))
-    .catch(() => []);
+    .catch((e) => {
+      console.error(e);
+      return {};
+    });
 
   return Response.json(data);
 }
