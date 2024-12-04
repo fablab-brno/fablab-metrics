276d36e4c9155f10b47e05df29cc61c8ae4212f6
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2024-12-04 14:15:00 +0100

    read api key from query params

--- app/api/refresh/route.ts
+++ app/api/refresh/route.ts
@@ -1,23 +1,24 @@
 import { hashPassword } from "fablab-metrics/auth/hasher";
 import { cookies } from "next/headers";
+import { type NextRequest } from "next/server";
 import { exec } from "node:child_process";
 
-export async function POST(req: Request) {
-  const data = await req.json().catch((err: Error) => ({}));
-
+export async function POST(req: NextRequest) {
   const hash = (await cookies()).get(process.env.COOKIE_AUTH!)?.value;
   const expected = await hashPassword(process.env.AUTH_PASSWORD!);
 
+  const key = req.nextUrl.searchParams.get("key");
+
   if (
-    (hash == null && data.key == null) ||
-    (hash && hash !== expected) ||
-    (data.key && data.key !== process.env.API_KEY)
+    (hash == null && key == null) ||
+    (hash != null && hash !== expected) ||
+    (key != null && key !== process.env.API_KEY)
   ) {
     return new Response(null, { status: 403 });
   }
 
   const output = await new Promise((resolve, reject) =>
-    exec("poetry run python dataloader/main.py", (err, stdout, stderr) => {
+    exec("python dataloader/main.py", (err, stdout, stderr) => {
       if (err) {
         console.error(err);
         reject({
